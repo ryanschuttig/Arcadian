156e8a97ef264376b678f66f324f6e5ab8cbf95d
Christopher Holmes <holmes.chris94@yahoo.com>
2020-08-19 15:42:05 -0400

    changed navbar text and radius

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import Logout from "./Logout";
-
+import "../components/Nav.css"
 export const NavBar = () => {
 
     const { currentUser, isAuthenticated } = useSelector(state => state.auth)
