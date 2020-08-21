8868f4350cb5409326428582a93d61b2b3eed672
Christopher Holmes <holmes.chris94@yahoo.com>
2020-08-21 12:36:49 -0400

    finished game functionality, finishing up styling

--- client/src/pages/TictacGamePage/Board.js
+++ client/src/pages/TictacGamePage/Board.js
@@ -2,31 +2,23 @@ import React from "react";
 import Square from "./Square"
 
 const style = {
-    border: "4px solid darkblue",
+    border: "4px solid #08F300",
     borderRadius: "10px",
-    width: "400px",
-    height: "400px",
-    margin: "0 auto",
+    width: "500px",
+    height: "500px",
+    margin: "auto",
+    marginTop: "40px",
     display: "grid",
     gridTemplate: "repeat(3, 1fr) / repeat(3, 1fr)"
 }
 
 
-const Board = ({ square, onClick }) => (
+const Board = ({ squares, onClick }) => (
     <div style={style}>
-        {squares.map((square, i) => {
-            <Square value="1" onClick={() => onClick("placeholder")} />
-        })}
+        {squares.map((square, i) => (
+            <Square key={i} value={square} onClick={() => onClick(i)} />
+        ))}
 
-        <Square value="1" onClick={() => onClick("placeholder")} />
-        <Square value="2" onClick={() => onClick("placeholder")} />
-        <Square value="3" onClick={() => onClick("placeholder")} />
-        <Square value="4" onClick={() => onClick("placeholder")} />
-        <Square value="5" onClick={() => onClick("placeholder")} />
-        <Square value="6" onClick={() => onClick("placeholder")} />
-        <Square value="7" onClick={() => onClick("placeholder")} />
-        <Square value="8" onClick={() => onClick("placeholder")} />
-        <Square value="9" onClick={() => onClick("placeholder")} />
     </div>
 )
 
