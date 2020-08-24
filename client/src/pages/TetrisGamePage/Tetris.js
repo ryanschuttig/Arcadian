af1de170a62b2b1e1cab0c4f80f0ae845cad2e9e
ryanschuttig <ryanschuttig@gmail.com>
2020-08-24 14:48:30 -0400

    scores

--- client/src/pages/TetrisGamePage/Tetris.js
+++ client/src/pages/TetrisGamePage/Tetris.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { createStage, checkCollision } from './gameHelpers';
 
 // Styled Components
@@ -14,6 +14,7 @@ import { useGameStatus } from './hooks/useGameStatus';
 import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
+import API from '../../utils/API';
 
 const Tetris = () => {
     const [dropTime, setDropTime] = useState(null);
@@ -24,6 +25,19 @@ const Tetris = () => {
     const [score, setScore, rows, setRows, level, setLevel, prevscore, setPrevScore] = useGameStatus(rowsCleared);
 
     console.log('re-render');
+
+    useEffect(() => {
+        getPrevScore()
+    }, [])
+
+    // API
+    function getPrevScore() {
+        API.getTetrisScore()
+        .then(res =>
+            setPrevScore(res.data)
+            )
+            .catch(err => console.log(err));
+    }
 
     const movePlayer = dir => {
         if (!checkCollision(player, stage, { x: dir, y: 0 })) {
@@ -40,7 +54,7 @@ const Tetris = () => {
         setScore(0);
         setRows(0);
         setLevel(0);
-        setPrevScore(0);
+        getPrevScore(0);
     };
 
     const drop = () => {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const authRoutes = require("./auth");
+const tetrisRoutes = require("./tetrisScore");
 const path = require("path");
 
 // Routes for authentication
 router.use("/auth", authRoutes);
+router.use("/api", tetrisRoutes);
 
 // If no API routes are hit, send the React app
 router.use("*", (req, res) => res.sendFile(path.join(__dirname, "../client/build/index.html")));
