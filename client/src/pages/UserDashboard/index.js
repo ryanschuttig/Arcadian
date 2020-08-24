5deee10bb9bfa29c65e68d95bf4d84bd174c9899
ryanschuttig <ryanschuttig@gmail.com>
2020-08-24 15:18:11 -0400

    good

--- client/src/pages/UserDashboard/index.js
+++ client/src/pages/UserDashboard/index.js
@@ -11,9 +11,8 @@ export const UserDashboard = () => {
     return (
         <>
             <Message className="message-container" size="huge" secondary="true">
-                <Header size="huge"> User Dashboard </Header>
-                <p>This is a Protected Route</p>
-                <p>Welcome {user ? user.email : ""}</p>
+                <Header size="huge" className="textneon"> User Dashboard </Header>
+                {/* <p>Welcome {user ? user.email : ""}!</p> */}
             </Message>
         </>
     )
