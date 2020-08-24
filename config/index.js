a6498310edaf9041c23b8036b0487d384072685d
ryanschuttig <ryanschuttig@gmail.com>
2020-08-24 16:42:34 -0400

    deploy

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 // Credentials for jwt Secret and Database
 module.exports = {
-    MONGO_URI: process.env.MONGODB_URI || "mongodb://root:rootroot1@ds021462.mlab.com:21462/heroku_jsc5306q",
+    MONGO_URI: process.env.MONGODB_URI || process.env.DATABASE_INFO,
     jwtSecret: process.env.JWT_SECRET
 }
