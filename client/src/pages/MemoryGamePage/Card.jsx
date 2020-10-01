1ae266bca8d6abe144a6830f5c90d579db626501
ryanschuttig <ryanschuttig@gmail.com>
2020-10-01 11:50:37 -0400

    fixes

--- client/src/pages/MemoryGamePage/Card.jsx
+++ client/src/pages/MemoryGamePage/Card.jsx
@@ -3,12 +3,10 @@ import ReactCardFlip from "react-card-flip";
 
 const Card = ({ id, isFlipped, handleClick, cardNumber }) => (
   <ReactCardFlip isFlipped={isFlipped} flipSpeedBackToFront={1} flipSpeedFrontToBack={1} >
-    <button id={id} className={`card card-front ${cardNumber !== -1 ? "" : "hide-card"}`} onClick={handleClick} key="front">
-
-    </button>
+    <button id={id} className={`card card-front ${cardNumber !== -1 ? "" : "hide-card"}`} onClick={handleClick} key="front"></button>
 
     <button id={id} className={`card card-back ${cardNumber !== -1 ? "" : "hide-card"}`} onClick={handleClick} key="back">
-      { cardNumber }
+      {cardNumber}
     </button>
   </ReactCardFlip>
 );
